8db29df31c271c24d27a4f8cd37844616bc06c66
Darwin <salinash2000@gmail.com>
2021-12-13 09:41:33 -0600

    athentication

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  async create(userDto: CreateUserDto) {
+    const user = this.repo.create({ ...userDto });
+    return this.repo.save(user);
+  }
+
+  async findOne(id: number) {
+    if (!id) {
+      return null;
+    }
+
+    const user = await this.repo.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  async find(email: string) {
+    const users = await this.repo.find({ email });
+    return users;
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return this.repo.remove(user);
+  }
+}
